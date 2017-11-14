5a24538ffa276330d08088ce2ecd13233e228074
darren <764190362@qq.com>
2017-11-14 12:52:56 +0800

    增加model validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,13 +9,28 @@ namespace RazorTutorial.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength =3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        //jquery validation不支持: [Range(typeof(DateTime), "1/a/1996", "1/a/2020")]不推荐使用
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
